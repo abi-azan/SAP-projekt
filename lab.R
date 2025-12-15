a0ac026d40ac438005f0bad43d09f976c7e9d104
lanavucen <lv56205@fer.hr>
2025-12-15 13:42:18 +0100

    potrebni podaci

--- lab.R
+++ lab.R
@@ -1,1 +1,23 @@
-print(prvo_pitanje2)
+winners <- oscars %>%
+  filter(Award=="Winner") %>%
+  count()
+
+winners_genres <- oscars %>%
+  filter(Award=="Winner") %>%
+  count(Main.Genre, sort=TRUE)
+
+prvo_pitanje <- chisq.test(winners_genres$n)
+
+genres <- oscars %>%
+  count(Main.Genre, sort=TRUE) 
+
+time <- oscars %>%
+  group_by(Main.Genre) %>%
+  summarise(Prosjek = mean(Movie.Time), Broj_Filmova = n()) %>%
+  arrange(desc(Prosjek))
+
+winners_producer <- oscars %>%
+  filter(Award=="Winner") %>%
+  separate_rows(Film.Studio.Producer.s.,sep= ",") %>%
+  count(Film.Studio.Producer.s., sort=TRUE)
+
