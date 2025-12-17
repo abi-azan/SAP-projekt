75284ad0dd16a4096021648cf640e11f8a4b173a
lanavucen <lv56205@fer.hr>
2025-12-17 15:23:26 +0100

    dodano 4 pitanje al je krivo

--- lab.R
+++ lab.R
@@ -1,22 +1,22 @@
-winners <- oscars %>%
+winners <- oscars_dataset %>%
   filter(Award=="Winner") %>%
   count()
 
-winners_genres <- oscars %>%
+winners_genres <- oscars_dataset %>%
   filter(Award=="Winner") %>%
   count(Main.Genre, sort=TRUE)
 
 prvo_pitanje <- chisq.test(winners_genres$n)
 
-genres <- oscars %>%
+genres <- oscars_dataset %>%
   count(Main.Genre, sort=TRUE) 
 
-time <- oscars %>%
+time <- oscars_dataset %>%
   group_by(Main.Genre) %>%
   summarise(Prosjek = mean(Movie.Time), Broj_Filmova = n()) %>%
   arrange(desc(Prosjek))
 
-winners_producer <- oscars %>%
+winners_producer <- oscars_dataset %>%
   filter(Award=="Winner") %>%
   separate_rows(Film.Studio.Producer.s.,sep= ",") %>%
   count(Film.Studio.Producer.s., sort=TRUE)
